8e934e8e9052920b4b9c4a634c09dba4b153f5a8
Tony Kim <tonykimatpl@gmail.com>
2017-12-02 18:09:40 -0800

    rt

--- master_1/master_1.ino
+++ master_1/master_1.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
 int buzzer=A1; 
 const int button1=1;   //team 1 button 
 bool buttonstate=0; 
@@ -21,6 +23,12 @@ pinMode(button2,INPUT);
 pinMode(button3,INPUT); 
 pinMode(button4,INPUT); 
 pinMode(reset,INPUT); 
+lcd.begin(16,2);
+lcd.print(" JeoParty v1.0");
+lcd.setCursor(0,1);
+lcd.print("by Tony and Alex");
+delay(2000);
+lcd.clear();
 } 
  
 void loop() { 
@@ -29,32 +37,50 @@ buttonstate2 = digitalRead(button2);
 buttonstate3 = digitalRead(button3); 
 buttonstate4 = digitalRead(button4); 
 buttonstate5 = digitalRead(reset); 
+lcd.noDisplay();
+lcd.display();
 while ((buttonstate == HIGH)||(buttonstate2 == HIGH)||(buttonstate3 == HIGH)||(buttonstate4 == HIGH)){ 
 if (buttonstate == HIGH){
   digitalWrite(led1,HIGH);
-  tone(buzzer, 400, 1000); 
+  tone(buzzer, 400, 1000);
+  lcd.print("     Team 1     ");
+ lcd.setCursor(0,1);
+ lcd.print("   HAS BUZZED");
   delay(8000);
+  lcd.clear();
   digitalWrite(led1,LOW);
   break;
 }
 else if(buttonstate2 == HIGH){
   digitalWrite(led2,HIGH);
   tone(buzzer, 400, 1000); 
+ lcd.print("     Team 2     ");
+ lcd.setCursor(0,1);
+ lcd.print("   HAS BUZZED");
   delay(8000);
+  lcd.clear();
   digitalWrite(led2,LOW);
   break;
 }
 else if(buttonstate3 == HIGH){
   digitalWrite(led3,HIGH);
   tone(buzzer, 400, 1000); 
+lcd.print("     Team 3     ");
+ lcd.setCursor(0,1);
+ lcd.print("   HAS BUZZED");
   delay(8000);
+  lcd.clear();
   digitalWrite(led3,LOW);
   break;
 }
 else if(buttonstate4 == HIGH){
   digitalWrite(led4,HIGH);
   tone(buzzer, 400, 1000); 
+ lcd.print("     Team 4     ");
+ lcd.setCursor(0,1);
+ lcd.print("   HAS BUZZED");
   delay(8000);
+  lcd.clear();
   digitalWrite(led4,LOW);
   break;
 }
